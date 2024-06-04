78f6274939cf4eb092fbbb2601c2fb9471fc5021
Denny Setyawan Huda <denycam9@gmail.com>
2024-06-05 00:22:35 +0700

    fix: fill layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,8 @@ const BaseLayout = () => {
 	return (
 		<Stack>
 			<Stack.Screen name="index" options={{ headerShown: false }} />
+			<Stack.Screen name="(auth)" options={{ headerShown: false }} />
+			<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
 		</Stack>
 	);
 };
