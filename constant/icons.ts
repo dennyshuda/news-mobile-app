992afa556197c7088053d5353407f927ec5846f6
Denny Setyawan Huda <denycam9@gmail.com>
2024-06-05 00:05:05 +0700

    chore: upload icons

--- constant/icons.ts
+++ constant/icons.ts
@@ -1,5 +1,7 @@
 import home from "../assets/icons/home.png";
 import plus from "../assets/icons/plus.png";
 import profile from "../assets/icons/profile.png";
+import eyeHide from "../assets/icons/eye-hide.png";
+import eye from "../assets/icons/eye.png";
 
-export default { home, plus, profile };
+export default { home, plus, profile, eyeHide, eye };
