2763b09a83702be5971b1298222e495a648a1f10
Denny Setyawan Huda <denycam9@gmail.com>
2024-06-04 19:39:54 +0700

    feat: create index page

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,26 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { router } from "expo-router";
+import { ScrollView, Text, View, Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import images from "../constant/images";
+import CustomButton from "../components/CustomButton";
 
 const App = () => {
 	return (
-		<View className="flex-1 items-center justify-center bg-white">
-			<Text className="text-3xl">News App</Text>
-			<Link href="/home">Go to Home</Link>
-		</View>
+		<SafeAreaView className="h-full bg-white">
+			<ScrollView contentContainerStyle={{ height: "100%" }}>
+				<View className="w-full justify-center items-center h-full px-4 gap-8">
+					<Text className="text-3xl text-dark-900 text-center font-bold">
+						Selamat Datang di Portal Berita Terpercaya
+					</Text>
+					<Image source={images.news} className="w-full max-w-[380px] h-[300px]" />
+					<Text className="text-center text-sm text-primary-600">
+						Kami hadir untuk memberikan Anda berita terbaru, terpercaya, dan terkini. Dari politik
+						hingga hiburan, kami meliput semua yang Anda butuhkan untuk tetap terinformasi.
+					</Text>
+					<CustomButton title="Mulai" onPress={() => router.push("/sign-in")} />
+				</View>
+			</ScrollView>
+		</SafeAreaView>
 	);
 };
 
