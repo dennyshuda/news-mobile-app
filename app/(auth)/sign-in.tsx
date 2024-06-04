4f9b5307bff264119511b8963ba7fea07e92ac37
Denny Setyawan Huda <denycam9@gmail.com>
2024-06-05 00:21:47 +0700

    feat: create sign-in and sign-up screen

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,10 +1,49 @@
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
+import { useState } from "react";
+import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import FormField from "../../components/FormField";
+import CustomButton from "../../components/CustomButton";
 
 const SignIn = () => {
+	const [form, setForm] = useState({ email: "", password: "" });
+
+	const submit = () => {
+		console.log(form);
+	};
+
 	return (
-		<View>
-			<Text>SignIn</Text>
-		</View>
+		<SafeAreaView style={{ backgroundColor: "white", height: "100%" }}>
+			<ScrollView>
+				<View className="w-full justify-center px-4 py-6 gap-3 min-h-[85vh]">
+					<View>
+						<Text className="text-2xl font-semibold text-dark-900">Welcome to, NewsApp</Text>
+						<Text className="text-lg text-primary-500">Enter your account to continue</Text>
+					</View>
+
+					<View className="gap-5">
+						<FormField
+							title="Email"
+							placeholder="Your email address"
+							keyboardType="email-address"
+							onChangeText={(text) => setForm({ ...form, email: text })}
+						/>
+						<FormField
+							title="Password"
+							placeholder="Your password"
+							onChangeText={(text) => setForm({ ...form, password: text })}
+						/>
+						<CustomButton title="Login" onPress={submit} />
+						<Text className="text-lg text-center">
+							Don't have account?{" "}
+							<Link href="/sign-up" className="font-semibold text-dark-900">
+								Sign Up
+							</Link>
+						</Text>
+					</View>
+				</View>
+			</ScrollView>
+		</SafeAreaView>
 	);
 };
 
--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,10 +1,54 @@
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
+import { useState } from "react";
+import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import FormField from "../../components/FormField";
+import CustomButton from "../../components/CustomButton";
 
 const SignUp = () => {
+	const [form, setForm] = useState({ name: "", email: "", password: "" });
+
+	const submit = async () => {
+		console.log(form);
+	};
+
 	return (
-		<View>
-			<Text>SignUp</Text>
-		</View>
+		<SafeAreaView style={{ backgroundColor: "white", height: "100%" }}>
+			<ScrollView>
+				<View className="w-full justify-center px-4 py-6 gap-3 min-h-[85vh]">
+					<View>
+						<Text className="text-2xl font-semibold text-dark-900">Nice to know you!</Text>
+						<Text className="text-lg text-primary-500">Enter your account to continue</Text>
+					</View>
+
+					<View className="gap-5">
+						<FormField
+							title="Name"
+							placeholder="Your name"
+							onChangeText={(text) => setForm({ ...form, name: text })}
+						/>
+						<FormField
+							title="Email"
+							placeholder="Your email address"
+							keyboardType="email-address"
+							onChangeText={(text) => setForm({ ...form, email: text })}
+						/>
+						<FormField
+							title="Password"
+							placeholder="Your password"
+							onChangeText={(text) => setForm({ ...form, password: text })}
+						/>
+						<CustomButton title="Register" onPress={submit} />
+						<Text className="text-lg text-center">
+							Have an account already?{" "}
+							<Link href="/sign-in" className="font-semibold text-dark-900">
+								Sign In
+							</Link>
+						</Text>
+					</View>
+				</View>
+			</ScrollView>
+		</SafeAreaView>
 	);
 };
 
