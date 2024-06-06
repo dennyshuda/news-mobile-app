1e0188a4b0998d6006afffd97db15752a9fc3445
Denny Setyawan Huda <denycam9@gmail.com>
2024-06-06 17:11:45 +0700

    fix: add bookmark tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -15,7 +15,7 @@ const TabIcon = ({ icon, color, name, focused }: TabIconProps) => {
 	return (
 		<View className="items-center justify-center gap-2">
 			<Image source={icon} resizeMode="contain" tintColor={color} className="w-6 h-6" />
-			<Text className={`${focused ? "font-semibold" : "font-normal"} text-xs`}>{name}</Text>
+			{/* <Text className={`${focused ? "font-semibold" : "font-normal"} text-xs`}>{name}</Text> */}
 		</View>
 	);
 };
@@ -53,12 +53,22 @@ const TabsLayout = () => (
 				}}
 			/>
 			<Tabs.Screen
+				name="bookmark"
+				options={{
+					title: "Bookmark",
+					headerShown: false,
+					tabBarIcon: ({ color, focused }: TabBarProps) => (
+						<TabIcon icon={icons.bookmark} color={color} name="Bookmark" focused={focused} />
+					),
+				}}
+			/>
+			<Tabs.Screen
 				name="profile"
 				options={{
 					title: "Profile",
 					headerShown: false,
 					tabBarIcon: ({ color, focused }: TabBarProps) => (
-						<TabIcon icon={icons.plus} color={color} name="Profile" focused={focused} />
+						<TabIcon icon={icons.profile} color={color} name="Profile" focused={focused} />
 					),
 				}}
 			/>
