31cfcbe8a8fde62de1b46003f0406f8503e9c7b7
Hafiyan Fazagi <hfiyanfazag1@gmail.com>
2024-06-06 21:05:47 +0700

    login frontend

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -6,7 +6,7 @@ import FormField from "../../components/FormField";
 import CustomButton from "../../components/CustomButton";
 
 const SignIn = () => {
-	const [form, setForm] = useState({ email: "", password: "" });
+	const [form, setForm] = useState({ nama: "", email: "", password: "" });
 
 	const submit = () => {
 		console.log(form);
@@ -15,31 +15,40 @@ const SignIn = () => {
 	return (
 		<SafeAreaView style={{ backgroundColor: "white", height: "100%" }}>
 			<ScrollView>
-				<View className="w-full justify-center px-4 py-6 gap-3 min-h-[85vh]">
-					<View>
-						<Text className="text-2xl font-semibold text-dark-900">Welcome to, NewsApp</Text>
-						<Text className="text-lg text-primary-500">Enter your account to continue</Text>
+				<View className="w-full justify-center px-4 py-6 gap-3">
+					<View className="flex items-center py-6">
+						<Text className="text-4xl font-bold text-dark-900">Login</Text>
+						{/* <Text className="text-lg text-primary-500">Enter your account to continue</Text> */}
 					</View>
 
-					<View className="gap-5">
+					<View className="gap-5 mt-16">
+						<FormField
+							title="Nama"
+							placeholder="Masukkan Nama"
+							// keyboardType="email-address"
+							onChangeText={(text) => setForm({ ...form, nama: text })}
+						/>
 						<FormField
 							title="Email"
-							placeholder="Your email address"
-							keyboardType="email-address"
+							placeholder="Masukkan Email"
+							// keyboardType="email-address"
 							onChangeText={(text) => setForm({ ...form, email: text })}
 						/>
 						<FormField
 							title="Password"
-							placeholder="Your password"
+							placeholder="Masukkan Password"
 							onChangeText={(text) => setForm({ ...form, password: text })}
+							className="w-fit"
 						/>
-						<CustomButton title="Login" onPress={submit} />
-						<Text className="text-lg text-center">
-							Don't have account?{" "}
-							<Link href="/sign-up" className="font-semibold text-dark-900">
-								Sign Up
-							</Link>
-						</Text>
+						<View className="mt-20">
+							<CustomButton title="Masuk" onPress={submit} />
+							<Text className="text-lg text-center">
+								Don't have account?{" "}
+								<Link href="/sign-up" className="font-semibold text-dark-900">
+									Sign Up
+								</Link>
+							</Text>
+						</View>
 					</View>
 				</View>
 			</ScrollView>
