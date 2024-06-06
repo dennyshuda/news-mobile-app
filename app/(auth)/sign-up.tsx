c6fba932c3d9a5996bc40c470c42bdf80f25537c
Hafiyan Fazagi <hfiyanfazag1@gmail.com>
2024-06-06 22:03:26 +0700

    signup frontend

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -2,26 +2,33 @@ import { ScrollView, Text, View } from "react-native";
 import { useState } from "react";
 import { Link } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { SelectList } from 'react-native-dropdown-select-list'
 import FormField from "../../components/FormField";
 import CustomButton from "../../components/CustomButton";
 
 const SignUp = () => {
 	const [form, setForm] = useState({ name: "", email: "", password: "" });
+	const [selected, setSelected] = useState("");
+
+	const data = [
+		{key:'1', value:'Pembaca'},
+		{key:'2', value:'Penulis'},
+	]
 
 	const submit = async () => {
-		console.log(form);
+		console.log(form, selected);
 	};
 
 	return (
 		<SafeAreaView style={{ backgroundColor: "white", height: "100%" }}>
 			<ScrollView>
-				<View className="w-full justify-center px-4 py-6 gap-3 min-h-[85vh]">
-					<View>
-						<Text className="text-2xl font-semibold text-dark-900">Nice to know you!</Text>
-						<Text className="text-lg text-primary-500">Enter your account to continue</Text>
+				<View className="w-full justify-center px-6 py-6 gap-3">
+				<View className="flex items-center py-6">
+						<Text className="text-4xl font-bold text-dark-900">Sign Up</Text>
+						{/* <Text className="text-lg text-primary-500">Enter your account to continue</Text> */}
 					</View>
 
-					<View className="gap-5">
+					<View className="gap-5 mt-16">
 						<FormField
 							title="Name"
 							placeholder="Your name"
@@ -38,13 +45,25 @@ const SignUp = () => {
 							placeholder="Your password"
 							onChangeText={(text) => setForm({ ...form, password: text })}
 						/>
-						<CustomButton title="Register" onPress={submit} />
-						<Text className="text-lg text-center">
-							Have an account already?{" "}
-							<Link href="/sign-in" className="font-semibold text-dark-900">
-								Sign In
-							</Link>
-						</Text>
+						<View className="gap-3">
+							<Text className="text-xl font-bold">Masuk Sebagai</Text>
+							<SelectList 
+									setSelected={(val: string) => setSelected(val)} 
+									data={data} 
+									save="value"
+									boxStyles={{height: 50, width: '100%'}}
+									inputStyles={{alignContent: 'center'}}
+							/>
+						</View>
+						<View className="mt-12">
+							<CustomButton title="Daftar" onPress={submit} />
+							<Text className="text-lg text-center">
+								Have an account already?{" "}
+								<Link href="/sign-in" className="font-semibold text-dark-900">
+									Sign In
+								</Link>
+							</Text>
+						</View>
 					</View>
 				</View>
 			</ScrollView>
