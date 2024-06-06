fa3cfe2c1062965f7b8442a537d6996acd3810ef
Denny Setyawan Huda <denycam9@gmail.com>
2024-06-06 17:10:20 +0700

    chore: add bookmark image

--- constant/icons.ts
+++ constant/icons.ts
@@ -3,5 +3,6 @@ import plus from "../assets/icons/plus.png";
 import profile from "../assets/icons/profile.png";
 import eyeHide from "../assets/icons/eye-hide.png";
 import eye from "../assets/icons/eye.png";
+import bookmark from "../assets/icons/bookmark.png";
 
-export default { home, plus, profile, eyeHide, eye };
+export default { home, plus, profile, eyeHide, eye, bookmark };
