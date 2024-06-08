620a93b95d62112c900d408ce33070f77cee6c76
Denny Setyawan Huda <denycam9@gmail.com>
2024-06-08 21:02:50 +0700

    feat: create profile screen

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,40 @@
-import { SafeAreaView, ScrollView, Text, View } from "react-native";
+import { SafeAreaView, ScrollView, Image, View, Text } from "react-native";
+import Nav from "../../components/Nav";
+import images from "../../constant/images";
+import CustomButton from "../../components/CustomButton";
 
 const Profile = () => {
 	return (
 		<SafeAreaView style={{ backgroundColor: "white", height: "100%" }}>
-			<ScrollView>
-				<Text>Profile</Text>
+			<ScrollView contentContainerStyle={{ height: "100%" }}>
+				<View className="px-4 pt-10">
+					<Nav showBookmark={false} />
+
+					<View className="h-[85vh] justify-between">
+						<View className="items-center gap-10">
+							<Image source={images.avatar} className="w-30 h-30" />
+
+							<View className="items-center">
+								<Text className="font-bold text-2xl text-lynch-900">Kobo Kanaeru</Text>
+								<Text className="text-lynch-400 text-lg">kobokanaeru@gmail.com</Text>
+							</View>
+
+							<View className="flex-row gap-5">
+								<View className="items-center gap-5">
+									<Text className="font-bold text-6xl">20</Text>
+									<Text className="text-lynch-400">Berita disimpan</Text>
+								</View>
+								<View className="w-[2px] bg-lynch-200 rounded-lg"></View>
+								<View className="items-center gap-5">
+									<Text className="font-bold text-6xl">20</Text>
+									<Text className="text-lynch-400">Berita dipulish</Text>
+								</View>
+							</View>
+						</View>
+
+						<CustomButton title="Keluar" />
+					</View>
+				</View>
 			</ScrollView>
 		</SafeAreaView>
 	);
