8bfa2c081ce2f033b79e36e60d81e1f87d788983
Denny Setyawan Huda <denycam9@gmail.com>
2024-06-09 23:04:16 +0700

    fix: add index image

--- constant/images.ts
+++ constant/images.ts
@@ -1,5 +1,6 @@
 import news from "../assets/images/news.png";
 import avatar from "../assets/images/avatar.png";
 import thumbnail from "../assets/images/thumbnail.png";
+import home from "../assets/images/home.png";
 
-export default { news, avatar, thumbnail };
+export default { news, avatar, thumbnail, home };
