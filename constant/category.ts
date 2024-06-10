bb184b5414588316b7b619b8c09bdbceff2716b2
Denny Setyawan Huda <denycam9@gmail.com>
2024-06-10 21:42:59 +0700

    chore: category constant

---
+++ constant/category.ts
@@ -0,0 +1,22 @@
+export const CATEGORY = [
+	{
+		key: "66632f736b196c35a0de2616",
+		value: "POLITIK",
+	},
+	{
+		key: "66632fd36b196c35a0de2617",
+		value: "KESEHATAN",
+	},
+	{
+		key: "6666f483b6885f0d8ec8eb00",
+		value: "TEKNOLOGI",
+	},
+	{
+		key: "6666f499b6885f0d8ec8eb01",
+		value: "AGAMA",
+	},
+	{
+		key: "6666f4edb6885f0d8ec8eb03",
+		value: "EKONOMI",
+	},
+];
