b73551415dcb79fa7b3fad1b3a82c1af3c7598ee
Denny Setyawan Huda <denycam9@gmail.com>
2024-06-10 16:15:27 +0700

    feat: create news card

---
+++ components/NewsCard.tsx
@@ -0,0 +1,30 @@
+import { Text, View, Image, Pressable } from "react-native";
+import images from "../constant/images";
+import { IPost } from "../services/interfaces/post.types";
+
+type NewsCardProps = {
+	post: IPost;
+};
+
+const NewsCard = ({ post }: NewsCardProps) => {
+	return (
+		<Pressable
+			onPress={() => console.log("s")}
+			className="rounded-xl border-[1px] border-lynch-400"
+		>
+			<View>
+				<Image source={images.thumbnail} className="w-full rounded-tr-xl rounded-tl-xl" />
+			</View>
+
+			<View className="p-3 gap-2">
+				<Text className="font-bold text-xl">{post.title}</Text>
+				<Text numberOfLines={2} className="text-lynch-400 text-ellipsis">
+					{post.content}
+				</Text>
+				<Text className="text-lynch-400 text-sm">14 Juni 2024</Text>
+			</View>
+		</Pressable>
+	);
+};
+
+export default NewsCard;
