f348f3b02d55a4fcf13a146dd3628da774de000e
Denny Setyawan Huda <denycam9@gmail.com>
2024-06-11 22:56:46 +0700

    refactor: using pressable

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,14 +1,20 @@
-import { Alert, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Alert, Image, Pressable, TextInput, View } from "react-native";
 import React, { useState } from "react";
 import { router, usePathname } from "expo-router";
+import icons from "../constant/icons";
 
 const SearchInput = () => {
 	const pathname = usePathname();
 	const [query, setQuery] = useState("");
 	return (
-		<View>
-			<TextInput placeholder="search" onChangeText={(value: string) => setQuery(value)} />
-			<TouchableOpacity
+		<View className="flex-row items-center justify-between border-[1px] rounded-full px-5 py-2 text-2xl border-lynch-400">
+			<TextInput
+				className="flex-1"
+				placeholder="search"
+				defaultValue={query}
+				onChangeText={(value: string) => setQuery(value)}
+			/>
+			<Pressable
 				onPress={() => {
 					if (!query) Alert.alert("please insery query");
 
@@ -19,8 +25,8 @@ const SearchInput = () => {
 					}
 				}}
 			>
-				<Text>search</Text>
-			</TouchableOpacity>
+				<Image source={icons.search} className="w-6 h-6" />
+			</Pressable>
 		</View>
 	);
 };
